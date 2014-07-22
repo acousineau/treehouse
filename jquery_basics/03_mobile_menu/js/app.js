5bbfd6a84cc1fa92269bcd1d9548aeb9e7585053
Andy Cousineau <andrew.cousineau@gmail.com>
2014-07-22 17:49:18 -0400

    Finish mobile menu

--- jquery_basics/03_mobile_menu/js/app.js
+++ jquery_basics/03_mobile_menu/js/app.js
@@ -9,6 +9,11 @@ $("#menu a").each(function(){
   var $anchor = $(this);
   //Create an option
   var $option = $('<option></option>');
+
+  //Deal with selected options depending on current page
+  if ($anchor.parent().hasClass("selected")) {
+    $option.prop("selected", true);
+  }
   //option's text is the text of link
   $option.text($anchor.text());
   //option's value is the href
@@ -24,12 +29,7 @@ $button.click(function(){
   //Go to select's location
   window.location = $select.val();
 });
-//Modify CSS to hide links on small widths and show button and select
-  //Also hides select and button on larger widths and show links
-//Deal with selected options depending on current page
 
-
-//LEFT OF AT PERFORM PART 3!!
 
 
 
