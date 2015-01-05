04042d3b21628bb1f0344962afd3721e531754cb
Andy Cousineau <andrew.cousineau@gmail.com>
2015-01-05 17:39:27 -0500

    JS and DOM Completion

---
+++ javascript/js_web_pages/01_JS_and_DOM/js/app.js
@@ -0,0 +1,35 @@
+var name = "Andy";
+
+console.log(window.location);
+console.log(window.name);
+console.log(window.document.body);
+
+console.log(document.forms);
+
+console.log(document.scripts);
+
+// Types of actions that can take place on the DOM
+// Mozilla Developer Network
+// mdn.mozilla.com
+
+/*
+1) 	Manipulation - where we manipulate elements
+2) 	Traversal - selecting an element based on the relationship with another
+	element. (For example, we could select a child element of a known parent
+	element like a list item child of an unordered list.)
+3) 	Events - listening to a specific event, like a mouse click or a key press,
+	and having something execute.
+*/
+
+// DOCUMENT
+// ELEMENT
+// NODE
+
+// 4 P's
+
+/*
+1) Preparation
+2) Plan
+3) Perform
+4) Perfect
+*/
